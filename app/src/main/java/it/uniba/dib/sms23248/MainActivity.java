81f403292ab8cad41150b3bd3e0b61ebb859427c
mprudente6 <prudentemaurizio@gmail.com>
2023-12-18 17:43:12 +0100

    Initial commit

---
+++ app/src/main/java/it/uniba/dib/sms23248/MainActivity.java
@@ -0,0 +1,20 @@
+package it.uniba.dib.sms23248;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
