2d6c8657178455db4f66a42bf2c6797d3420be5c
martinaminiello <94961059+martinaminiello@users.noreply.github.com>
2024-02-06 11:14:32 +0100

    ProfiloActivity layout

--- app/src/main/java/it/uniba/dib/sms23248/ProfiloActivity.java
+++ app/src/main/java/it/uniba/dib/sms23248/ProfiloActivity.java
@@ -2,31 +2,42 @@ package it.uniba.dib.sms23248;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import it.uniba.dib.sms23248.SpeseRichiedente.Spese;
 
 public class ProfiloActivity extends AppCompatActivity {
+    TextView nomeUtente;
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profilo);
 
-        // Find the 'Scheda anagrafica' button by its ID
+         nomeUtente=findViewById(R.id.textnomeUtente);
         ImageView schedaAnagrafica = findViewById(R.id.btnSchedaUser);
         ImageView bilancioSpese = findViewById(R.id.btnSpeseRichiedente);
 
-        // Set a click listener for the 'Scheda anagrafica' button
+        ProfileName();
+
         schedaAnagrafica.setOnClickListener(view -> {
-            // Create an intent to navigate to AnagraficaActivity
+
             Intent intent = new Intent(ProfiloActivity.this, AnagraficaActivity.class);
 
-            // Start the AnagraficaActivity
+
             startActivity(intent);
         });
 
@@ -38,23 +49,47 @@ public class ProfiloActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        // Find the 'Valuta l'app' text view by its ID
+
         CardView valutazione = findViewById(R.id.valutazioneApp);
 
-        // Set a click listener for the 'Valuta l'app' text view
+
         valutazione.setOnClickListener(view -> {
-            // Create an intent to navigate to ValutazioneActivity
+
             Intent intent = new Intent(ProfiloActivity.this, ValutazioneActivity.class);
 
-            // Start the ValutazioneActivity
+
             startActivity(intent);
         });
     }
 
-    // Method to go back to Home Screen
+
     public void goToHomeScreen() {
         Intent intent = new Intent(this, HomeR.class);
         startActivity(intent);
-        finish(); // Finish this activity when going back to Home Screen
+        finish();
+    }
+
+    private void ProfileName(){
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser= mAuth.getCurrentUser();
+        String uid= currentUser.getUid();
+
+        Task<DocumentSnapshot> Utente= db.collection("RICHIEDENTI_ASILO").document(uid).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+
+                                String nome = document.getString("Nome");
+                                String cognome = document.getString("Cognome");
+
+
+                                nomeUtente.setText( nome+" "+cognome );
+                            }
+                        }
+                    }
+                });
     }
 }
