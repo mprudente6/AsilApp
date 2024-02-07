05ff0b44e4394fd19f24710f72ae8c51eebaf449
martinaminiello <94961059+martinaminiello@users.noreply.github.com>
2024-02-07 11:30:32 +0100

    Layout Salute migliorati

--- app/src/main/java/it/uniba/dib/sms23248/pwContenitore.java
+++ app/src/main/java/it/uniba/dib/sms23248/pwContenitore.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
